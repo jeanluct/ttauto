1c363c774b09c851f0297e7ee488e6ed2c0f10ad
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2014-12-25 14:57:19 -0500

    Get rid of old 'doublehump' code in test_badwords.

--- tests/test_badwords.cpp
+++ tests/test_badwords.cpp
@@ -25,13 +25,9 @@
 #include <iostream>
 #include <list>
 #include <jlt/matrix.hpp>
-#ifdef DOUBLEHUMP_TRACK
-#include "doublehump.hpp"
-#else
 #include "traintrack.hpp"
 #include "ttfoldgraph.hpp"
 #include "ttauto.hpp"
-#endif
 #include "traintracks_util.hpp"
 #include "folding_path.hpp"
 #include "ttauto.hpp"
@@ -45,18 +41,6 @@ int main()
   using ttauto::traintrack;
   using ttauto::folding_path;
 
-#ifdef DOUBLEHUMP_TRACK
-  typedef doublehump TrTr;
-  doublehump tt(1,3,1, 1,1);
-#if 1
-  // Append a 3-prong to the 1st free prong of b branch, then another 3-prong.
-  tt.b.append(1,3);//->append(1,3);//->append(1,3)->append(1,3)->append(1,3);
-  // Append a 3-prong to the 2nd free prong of b branch, then another 3-prong.
-  //tt.b.append(2,3);//->append(1,3);
-#endif
-
-  tt.check();
-#else
   typedef traintrack TrTr;
 #if 1
   const int n = 6;
@@ -66,7 +50,6 @@ int main()
   const int n = 5; // Ham&Song
   traintrack tt(n,3,3);
   int maxplen = 6;
-#endif
 #endif
 
   typedef std::list<folding_path<TrTr> >::const_iterator path_it;
